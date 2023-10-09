ca96011fc404cf8035853850623b2447f3fe40e7
gin280 <zxy_wtfm@163.com>
2023-10-09 11:21:58 +0800

    update: README.md

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web"
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure(`e9db6c7d-238a-4c1e-bb3c-09c59ff43fc1`)
+    Crisp.configure(`${process.env.NEXT_PUBLIC_RISP_WEBSITE_ID}`)
   }, [])
 
   return null
